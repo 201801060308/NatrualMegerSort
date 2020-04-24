cd7487cb58559b80e77b870b3a001c2a308e8e2c
201801060308 <46990326+201801060308@users.noreply.github.com>
2020-04-24 17:20:58 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int length;   //元素个数
+    cin>>length;
+
+    int array[length];
+    for(int i = 0; i < length; i++)
+        cin>>array[i];
+
+
+}
